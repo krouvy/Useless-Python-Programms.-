737da94f2db5530d5bc93bcc28d9781a6606b1f7
krouvy <69346856+krouvy@users.noreply.github.com>
2021-03-25 22:22:57 +0300

    Add files via upload

---
+++ 2 - Hello/Hello.py
@@ -0,0 +1,8 @@
+print("hello world") 
+
+"""
+Yes, it's hello world in Python.
+function print() displays content of function.
+Don't forget about "apostrophes". 
+By the way, " " " or # helps to leave comments,
+"""
