3f7cd3672b56370de40bed0bed24d48104cc0315
krouvy <69346856+krouvy@users.noreply.github.com>
2021-03-25 22:50:07 +0300

    Add files via upload

---
+++ 3 - Math operators and strings/work with strings.py
@@ -0,0 +1,8 @@
+print("Hel", end="")  # function print has arguments, end and sep
+print("lo")           # default end="\n" line break and sep=" " space. But we can change it.
+dollars = 10
+cent = 99
+print("I have a", dollars, "dollars and", cent, "cent,", end="sad")
+
+# function print can take as many arguments as you like. The main thing is to use commas.
+
