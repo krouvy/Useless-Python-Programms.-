40db3de95d29695119d7c8a78dd069024b5e8e9f
AL <69346856+krouvy@users.noreply.github.com>
2021-03-26 19:07:14 +0300

    Add files via upload

---
+++ 7 - Strings/cycle_and_string.py
@@ -0,0 +1,7 @@
+s = "hello world"  # string hello world
+i = 0  # I still don't know about structure "for i in"
+while (i < len(s)):  # so I write code like this, while i lower then length s
+    print(s[i], end=" ")  # result       h e l l o   w o r l d
+    i += 1  # it's my analogue cycle for.
+
+# function len() return length of object.
---
+++ 7 - Strings/examples_of_strings.py
@@ -0,0 +1,11 @@
+str1 = ''' 
+It's super-puper 
+longer string, that
+can save inside not only 
+space break, but line break too.
+'''
+
+# This way you can no only write commentary, but write long strings with line break and so on.
+
+str2 = "normal string"
+print(str1, str2)
