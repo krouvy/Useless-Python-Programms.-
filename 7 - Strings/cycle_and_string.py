e74322beb2985e75babf5d644aa5b47c6fe69125
AL <69346856+krouvy@users.noreply.github.com>
2021-03-26 19:07:49 +0300

    Update cycle_and_string.py

--- 7 - Strings/cycle_and_string.py
+++ 7 - Strings/cycle_and_string.py
@@ -1,6 +1,6 @@
 s = "hello world"  # string hello world
 i = 0  # I still don't know about structure "for i in"
-while (i < len(s)):  # so I write code like this, while i lower then length s
+while (i < len(s)):  # so I write code like this, while i less then length s
     print(s[i], end=" ")  # result       h e l l o   w o r l d
     i += 1  # it's my analogue cycle for.
 
