204f635c292a9ab6ee7fce011876d12d15fbe170
AL <69346856+krouvy@users.noreply.github.com>
2021-03-26 17:24:42 +0300

    Add files via upload

---
+++ 4 - Assignment/Multiple_assignment.py
@@ -0,0 +1,4 @@
+a = b = c = d = 1  # Multiple assignment. In this example all variables get the value 1
+print(a, b, c, d)
+a, b, c, d = 2, 7, 1, 7  # In this example variables get the values 2,7,1,7
+print(a, b, c, d)
