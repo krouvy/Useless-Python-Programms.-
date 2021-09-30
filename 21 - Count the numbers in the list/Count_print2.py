ca4bfa74f0b8eaaae833d1418f162d832c088000
AL <69346856+krouvy@users.noreply.github.com>
2021-09-30 13:53:52 +0300

    Add files via upload

---
+++ 21 - Count the numbers in the list/Count_print2.py
@@ -0,0 +1,16 @@
+"""
+This is same program which counts how
+many times certain numbers
+appear in the list, but another way
+information output.
+Number range from 0 to 100
+"""
+
+Num_list = [4, 2, 4, 1, 2, 2, 3, 1, 1, 2, 3, 2, 0, 0, 0, 0, 4, 2, 5, 3, 6, 67, 54, 34, 23, 6, 4, 3, 5]
+Count_list = [0] * 100  # Creation of a list containing 100 zero cells
+for i in Num_list:  # the loop goes through all the elements of the Num_list
+    Count_list[i] += 1  # and using the Num_list number as index of count_list adds 1
+for i in range(len(Count_list)):  # then the loop goes through all index of the Num_list from 0 to 99
+    if Count_list[i] > 0:  # and if element of Count_list > 0
+        print((str(i) + " ") * Count_list[i], " ", end="")  # Convert the index to a string add "space"  and multiply
+print("\nEnd")                                              # by the Count_list element, and print without line break
